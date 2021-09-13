9521783f08de3ad3142b4dccf50e048ce7e76c5d
Luke Dulworth <lukedulworth2@gmail.com>
2021-09-13 11:37:06 -0500

    homework 0 submission

---
+++ homework-0/homework-0.py
@@ -0,0 +1,17 @@
+number_1 = 500
+number_2 = 100
+number_3 = 3.555
+country = "USA"
+county = "harris"
+print(number_1)
+print(number_2)
+print(number_3)
+print(country)
+print(county)
+print(type(number_1))
+print(type(number_2))
+print(type(number_3))
+print(type(country))
+print(type(county))
+
+
