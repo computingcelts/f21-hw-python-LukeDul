c270f7b97dd22bf9667200f07ec89e21fa519f7f
Luke Dulworth <lukedulworth2@gmail.com>
2021-09-21 12:26:14 -0500

    In class work submission

--- classwork/cwork03.py
+++ classwork/cwork03.py
@@ -1,2 +1,15 @@
 # In class work
-# Date:
+# Date: 9/21/21
+
+# User input
+x = int(input('enter number one: '))
+y = int(input('enter number two: '))
+z = input('enter a string: ')
+
+# Relational operators
+print(x < y)
+print(x > y)
+print(x <= y)
+print(x >= y)
+print(x == y)
+print(x != y)
