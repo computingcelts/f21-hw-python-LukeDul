b7c97a702e9ecd677feb91b68eabf3a671892da4
Luke Dulworth <lukedulworth2@gmail.com>
2021-09-21 11:59:57 -0500

    Added comments.

--- homework-0/homework-0.py
+++ homework-0/homework-0.py
@@ -1,13 +1,20 @@
+# Homework 0 | Luke Dulworth | 9/9/21 | COMSC 1450 A
+
+# Assigns variables.
 number_1 = 500
 number_2 = 100
 number_3 = 3.555
 country = "USA"
 county = "harris"
+
+# Prints variables.
 print(number_1)
 print(number_2)
 print(number_3)
 print(country)
 print(county)
+
+# Prints type of variables.
 print(type(number_1))
 print(type(number_2))
 print(type(number_3))
