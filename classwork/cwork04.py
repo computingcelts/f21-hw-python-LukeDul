dd03bd1931f5623bbc7503c732abfeb85542bce1
Luke Dulworth <lukedulworth2@gmail.com>
2021-09-28 12:34:12 -0500

    In class work 9/28/21

--- classwork/cwork04.py
+++ classwork/cwork04.py
@@ -2,14 +2,29 @@
 # Date: 9/28/21
 
 # enter value 1(a number) and store it in variable x
-x = int(input('Enter val1: '))
+x = int(input('Enter first value: '))
 
 # enter value 2(a number) and store it in variable y
-y = int(input('Enter val2: '))
+y = int(input('Enter second value: '))
 
 # enter value 3(a string) and store it in variable z
 z = input('Enter operator: ')
 
 # print the sum of x and y only if the value of z is equal to '+'
+
+print('Inputted operator: ', z)
 if z == '+':
     print(x + y)
+else:
+    if z == '-':
+        print(x - y)
+    else:
+        if z == '*':
+            print(x * y)
+        else:
+            if z == '/':
+                print(x / y)
+            else:
+                print('Invalid arithmetic operator.')
+
+
