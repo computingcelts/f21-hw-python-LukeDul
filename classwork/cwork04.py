1c4a047cff27ad1918b6790fe10ce14cedee45ef
Luke Dulworth <lukedulworth2@gmail.com>
2021-09-28 12:11:09 -0500

    In class work submission

--- classwork/cwork04.py
+++ classwork/cwork04.py
@@ -1,2 +1,12 @@
 # In class work
-# Date:
+# Date: 9/28/21
+
+# enter value 1(a number) and store it in variable x
+x = int(input('Enter val1: '))
+# enter value 2(a number) and store it in variable y
+y = int(input('Enter val2: '))
+# enter value 3(a string) and store it in variable z
+z = input('Enter operator: ')
+# print the sum of x and y only if the value of z is equal to '+'
+if z == '+':
+    print(x + y)
