ec08170b32bc524823ecced472630d517c32285c
Luke Dulworth <lukedulworth2@gmail.com>
2021-09-30 11:56:22 -0500

    In class work 9/30/21

--- classwork/cwork05.py
+++ classwork/cwork05.py
@@ -1,2 +1,27 @@
 # In class work
-# Date:
+# Date: 9/30/21
+
+x = input('Enter number between one and ten in alphabetic form: ')
+
+if x == 'one':
+    print('The numeric form of', x, 'is', 1)
+elif x == 'two':
+    print('The numeric form of', x, 'is', 2)
+elif x == 'three':
+    print('The numeric form of', x, 'is', 3)
+elif x == 'four':
+    print('The numeric form of', x, 'is', 4)
+elif x == 'five':
+    print('The numeric form of', x, 'is', 5)
+elif x == 'six':
+    print('The numeric form of', x, 'is', 6)
+elif x == 'seven':
+    print('The numeric form of', x, 'is', 7)
+elif x == 'eight':
+    print('The numeric form of', x, 'is', 8)
+elif x == 'nine':
+    print('The numeric form of', x, 'is', 9)
+elif x == 'ten':
+    print('The numeric form of', x, 'is', 10)
+else:
+    print("-------------------Invalid Input--------------------")
