54408832767ca3caaac18c15df4b010a6eacc5c8
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-05 12:25:53 -0500

    In class work 10/5/21

--- classwork/cwork06.py
+++ classwork/cwork06.py
@@ -1,10 +1,13 @@
 # In class work
 # Date: 10/5/21
 
+# Prompts user to input
 times = int(input('Enter amount of desired loops: '))
 
+# Initializes counter
 counter = 1
 
+# Counts to inputted value
 while counter <= times:
     print(counter)
     counter += 1
