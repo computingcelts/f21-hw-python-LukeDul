06ba35e077a27928b0fb15dd27db7994cc874f49
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-19 11:45:49 -0500

    In class work 10/19/21

--- classwork/cwork08.py
+++ classwork/cwork08.py
@@ -3,7 +3,7 @@
 done = False
 while not done:  # The operator not yields True if its argument is false, False otherwise
     entry = int(input('Please enter a number, odd if you want to finish: '))
-    if entry % 2 == 1:
+    if entry % 2 != 0:
         done = True
     else:
         print(entry)
