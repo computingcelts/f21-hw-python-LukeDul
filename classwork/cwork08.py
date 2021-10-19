dd348f1c14fbc1fa61498b4cd8a5236a640e98a0
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-19 11:34:11 -0500

    In class work 10/19/21

--- classwork/cwork08.py
+++ classwork/cwork08.py
@@ -1,7 +1,7 @@
 # In class work
 # Date: 10/19/21
 done = False
-while not done:
+while not done:  # The operator not yields True if its argument is false, False otherwise
     entry = int(input('Please enter a number, even if you want to finish: '))
     if entry % 2 == 0:
         done = True
