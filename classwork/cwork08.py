a704cc9b24ac70e9ed7268172c55dae31d870b3c
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-19 11:30:01 -0500

    In class work 10/19/21

--- classwork/cwork08.py
+++ classwork/cwork08.py
@@ -2,8 +2,8 @@
 # Date: 10/19/21
 done = False
 while not done:
-    entry = int(input('Please enter a number, positive if you want to finish: '))
-    if entry > 0:
+    entry = int(input('Please enter a number, negative if you want to finish: '))
+    if entry < 0:
         done = True
     else:
         print(entry)
