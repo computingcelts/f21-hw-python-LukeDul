a1bebd427f332205e8f4d92c5aa14defd4c50600
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-19 11:19:15 -0500

    In class work 10/19/21

--- classwork/cwork08.py
+++ classwork/cwork08.py
@@ -1,2 +1,9 @@
 # In class work
-# Date:
+# Date: 10/19/21
+done = False
+while not done:
+    entry = int(input('Please enter a number, 999 if you want to finish'))
+    if entry == 999:
+        done = True
+    else:
+        print(entry)
