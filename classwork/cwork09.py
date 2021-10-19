fb25ddcb82c2d5c9515ea78aef67020be0db8af9
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-19 12:40:53 -0500

    In class work 10/19/21

--- classwork/cwork09.py
+++ classwork/cwork09.py
@@ -1,2 +1,42 @@
 # In class work
-# Date:
+# Date: 10/19/21
+print('multiplication table')
+for m in range(1, 11):
+    for n in range(1, 11, 1):
+        print(n, "*", m, '=', n*m)
+
+print('example 1')
+for n in range(10, 101, 10):
+    print(n, end=', ')
+
+print('\nexample 2')
+for n in range(1, 10):
+    print(n, end=', ')
+
+print('\nexample 3')
+for n in range(1, 10, 2):
+    print(n, end=', ')
+
+print('\nexample 4')
+for n in range(10, 0, -1):
+    print(n, end=', ')
+
+print('\nexample 5')
+for n in range(10, 0, -2):
+    print(n, end=', ')
+
+print('\nexample 6')
+for n in range(2, 11, 2):
+    print(n, end=', ')
+
+print('\nexample 7')
+for n in range(-5, 5):
+    print(n, end=', ')
+
+print('\nexample 8')
+for n in range(1, 1):
+    print(n, end=', ')
+
+for letter in 'luke':
+    print(letter, end='')
+
