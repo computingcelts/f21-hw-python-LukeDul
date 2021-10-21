30e6314a264baed78f672afaea2c50950ef0a6bf
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-21 11:50:25 -0500

    In class work 10/21/21

--- classwork/cwork10.py
+++ classwork/cwork10.py
@@ -1,9 +1,17 @@
 # In class work
 # Date: 10/21/21
 
-sum = 0
+var = 0
+count = 0
 
 for i in range(1, 100):
-    if i % 2 == 0:
-        sum += i
-print(sum)
+    if i % 2 == 1:
+        var += i
+    if i > 15:
+        count += 1
+print('Accumulated odd numbers from 1 to 99:', var)
+print('Count of numbers from 15 to 99:', count)
+
+
+
+
