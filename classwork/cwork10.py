4c86fbfa6a22bad18558648bfa09a464125d9141
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-21 11:34:32 -0500

    In class work 10/21/21

--- classwork/cwork10.py
+++ classwork/cwork10.py
@@ -1,9 +1,9 @@
 # In class work
 # Date: 10/21/21
 
-# This is called an accumulator or aggregator.
 sum = 0
 
 for i in range(1, 100):
-    sum += i
+    if i % 2 == 0:
+        sum += i
 print(sum)
