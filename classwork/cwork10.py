18ba4329f9e8a62b8eee6d69c45df04ca06b7ab8
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-21 13:02:17 -0500

    In class work 10/21/21

--- classwork/cwork10.py
+++ classwork/cwork10.py
@@ -4,9 +4,9 @@
 var = 0
 count = 0
 
-for i in range(1, 100):
+for i in range(1, 10):
     if i % 2 == 1:
         var += i
         count += 1
-print('Count of odd numbers from 1 to 99:', count)
-print('Accumulated odd numbers from 1 to 99:', var)
+print('Count of odd numbers from 1 to 9:', count)
+print('Accumulated odd numbers from 1 to 9:', var)
