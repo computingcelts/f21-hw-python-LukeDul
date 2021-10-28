7df1dc52d5c4ec068bdb7fb0c5ca4720b8c333ba
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-28 11:57:13 -0500

    In class work 10/28/21

--- classwork/cwork12.py
+++ classwork/cwork12.py
@@ -1,10 +1,22 @@
 # In class work
 # Date: 10/28/21
-
-def my_function(a, b, c=1):
-    return a * b + c
+import pyttsx3
 
 
-print(my_function(1, 2))
-print(my_function(1, 2, 3))
-print(my_function(1, 2, c=5))
+def my_function(number_1, number_2, number_type='even'):
+    if number_1 % 2 == 0 and number_2 % 2 == 0:  # compound boolean expression
+        print('even')
+        return number_1 * number_2
+    elif number_1 % 2 == 1 and number_2 % 2 == 1:  # compound boolean expression
+        print('odd')
+        return number_1 + number_2
+
+
+engine = pyttsx3.init()
+engine.say("Hello")
+engine.runAndWait()
+
+
+print(my_function(2, 2))
+print(my_function(1, 1))
+
