c86408ddb3ee441729a1c01b8028529bed200fe1
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-28 12:12:53 -0500

    In class work 10/28/21

--- classwork/cwork12.py
+++ classwork/cwork12.py
@@ -3,20 +3,25 @@
 import pyttsx3
 
 
-def my_function(number_1, number_2, numbers_type='even'):
+def my_function(number_1, number_2, say_result='yes'):
     if number_1 % 2 == 0 and number_2 % 2 == 0:  # compound boolean expression
-        return number_1 * number_2
+        result = number_1 * number_2
+        if say_result == 'yes':
+            engine = pyttsx3.init()
+            engine.say('result')
+            engine.runAndWait()
+        return result
     else:
-        return number_1 + number_2
-
-
-engine = pyttsx3.init()
-engine.say("Hello")
-engine.runAndWait()
+        result = number_1 + number_2
+        if say_result == 'yes':
+            engine = pyttsx3.init()
+            engine.say('result')
+            engine.runAndWait()
+        return result
 
 
 print(my_function(1, 3))
 print(my_function(4, 8))
-print(my_function(1, 3, numbers_type='even'))
-print(my_function(1, 3, numbers_type='odd'))
+print(my_function(1, 3, say_result='yes'))
+print(my_function(1, 3, say_result='no'))
 
