49adf52bdbabe9ed643c444d59405c2d4a060491
Luke Dulworth <lukedulworth2@gmail.com>
2021-10-28 11:42:41 -0500

    In class work 10/28/21

--- classwork/cwork12.py
+++ classwork/cwork12.py
@@ -1,2 +1,10 @@
 # In class work
-# Date:
+# Date: 10/28/21
+
+def my_function(a, b, c=1):
+    return a * b + c
+
+
+print(my_function(1, 2))
+print(my_function(1, 2, 3))
+print(my_function(1, 2, c=5))
