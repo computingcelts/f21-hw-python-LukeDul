17e7004ec4d27829f3bd0c57d26f811071b0fd76
Luke Dulworth <lukedulworth2@gmail.com>
2021-11-09 12:31:28 -0600

    In class work 11/9/21

--- classwork/cwork14.py
+++ classwork/cwork14.py
@@ -1,2 +1,18 @@
 # In class work
-# Date:
+# Date: 11/9/21
+
+fruit = ['orange', 'apple', 'pear', 'banana', 'lemon']  # list because of [], mutable
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+index = 0
+
+print(fruit)
+
+for element in fruit:
+    if element[0] in vowels:  # in is a keyword that can iterate through a containers
+        fruit[index] = element.upper()
+        print(element.upper())
+    index += 1
+
+print(fruit)
