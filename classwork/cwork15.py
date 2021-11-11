91b1c7904bff8685d341fe44f558d97e0903c231
Luke Dulworth <lukedulworth2@gmail.com>
2021-11-11 12:37:45 -0600

    In class work 11/11/21

--- classwork/cwork15.py
+++ classwork/cwork15.py
@@ -1,8 +1,6 @@
 # In class work
 # Date: 11/11/15
-
 import pprint
-
 
 d = {}
 
@@ -12,5 +10,16 @@ d['Spain'] = 'Madrid'
 d['Mexico'] = 'CDX'
 
 print(d)
-print('Better Print!')
-pprint.pprint(d)  # not working :(
+
+d['Spain'] = ['Madrid', 'Barcelona']
+
+print(d)
+
+for country, cities in d.items():
+    print('key: ', country, 'and value: ', cities)
+
+print()  # these are loops do the exact same thing
+
+for x in d.items():
+    print('key: ', x[0], 'and value: ', x[1])
+
