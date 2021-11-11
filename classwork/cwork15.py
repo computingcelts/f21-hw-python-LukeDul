d4f3b9989df30781443dcd741cd3a5d40458c9d5
Luke Dulworth <lukedulworth2@gmail.com>
2021-11-11 12:17:09 -0600

    In class work 11/11/21

--- classwork/cwork15.py
+++ classwork/cwork15.py
@@ -1,2 +1,16 @@
 # In class work
-# Date:
+# Date: 11/11/15
+
+import pprint
+
+
+d = {}
+
+d['Italy'] = 'Rome'
+d['France'] = 'Paris'
+d['Spain'] = 'Madrid'
+d['Mexico'] = 'CDX'
+
+print(d)
+print('Better Print!')
+pprint.pprint(d)  # not working :(
