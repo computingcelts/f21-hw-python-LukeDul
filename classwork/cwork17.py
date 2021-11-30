d855de5bd28f759e89c69b73e52ea4759f973b7f
Luke Dulworth <lukedulworth2@gmail.com>
2021-11-30 12:36:23 -0600

    In class work 11/30/21

--- classwork/cwork17.py
+++ classwork/cwork17.py
@@ -1,8 +1,25 @@
 # In class work
 # Date: 11/30/21
 
+# Error Handling: Try and Except
+# Error Classes
+list1 = [3, 5, 6]
+d = {'Texas': 10, 'Arizona': 20}
+
 try:
     val = int(input('Please enter a small positive integer: '))
-    print('You entered:', val)
-except:
-    print('Input not accepted')
+
+    print('You entered :', val)
+    print('Element at', val, ':', list1[val])
+    print('The value of Texas is ', d['texas'])
+
+except IndexError:
+    print('Index invalid.')
+
+except ValueError:
+    print('Input not accepted.')
+
+except KeyError:
+    print('The key does\'nt exist.')
+
+print('Work done.')
